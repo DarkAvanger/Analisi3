00262b1e7d373295f0c4894698674a86d200e5fe
DarkAvanger <awesomekoala19@gmail.com>
2024-01-08 14:36:22 +0100

    Data sent

--- Assets/Data.cs
+++ Assets/Data.cs
@@ -1,15 +1,30 @@
 using Gamekit3D;
 using Gamekit3D.Message;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.Networking;
 using static Gamekit3D.Damageable;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
+
+public class EllenData
+{
+    public Vector3 position;
+    public int ranged;
+    public string enemy;
+}
 public class Data : MonoBehaviour, IMessageReceiver
 {
+    int rangedInt = 0;
+    public string serverUrl;
     public GameObject Ellen;
     public GameObject[] Enemies;
     Vector3 EllenPos;
+    Vector3 EllenMov;
 
     private void OnEnable()
     {
@@ -21,23 +36,85 @@ public class Data : MonoBehaviour, IMessageReceiver
 
         Damageable dam = (Damageable)sender;
         DamageMessage damMsg = (DamageMessage)msg;
+
         
+
         switch (type)
         {
             case MessageType.DAMAGED:
                 {
-                    EllenPos = dam.transform.position;
-                    Debug.Log("Ellen Damaged at: " + EllenPos + " by: "+ damMsg.damager.name);
+                    //EllenPos = dam.transform.position;
+                    //Debug.Log("Ellen Damaged at: " + EllenPos + " by: "+ damMsg.damager.name);
+                    //Debug.Log( "Melee or ranged: " + damMsg.throwing);
+                    //Debug.Log("From: " + damMsg.direction);
+                    //Debug.Log("From: " + damMsg.damageSource);
+                    //Debug.Log("Amount: " + damMsg.amount);
+                    if (damMsg.throwing)
+                    {
+                        rangedInt = 1;
+                    }
+                    EllenData user = new EllenData
+                    {
+                        position = dam.transform.position,
+                        ranged = rangedInt,
+                        enemy = damMsg.damager.name,
+                    };
+
+                    string jsonData = JsonUtility.ToJson(user);
+
+                    StartCoroutine(Upload(jsonData));
+
                 }
                 break;
             case MessageType.DEAD:
                 {
-                    EllenPos = Ellen.transform.position;
-                    Debug.Log("Ellen Dead at: " + EllenPos);
+                   // EllenPos = dam.transform.position;
+                    //Debug.Log("Ellen Dead at: " + EllenPos + " by: " + damMsg.damager.name);
+                    //Debug.Log("Melee or ranged: " + damMsg.throwing);
+                    //Debug.Log("From: " + damMsg.direction);
+                    //Debug.Log("From: " + damMsg.damageSource);
+                    //Debug.Log("Amount: " + damMsg.amount);
+                    EllenData user = new EllenData
+                    {
+                        position = dam.transform.position,
+                        ranged = rangedInt,
+                        enemy = damMsg.damager.name,
+                    };
+
+                    string jsonData = JsonUtility.ToJson(user);
                 }
                 break;
         }
     }
 
+    void Update()
+    {
+        EllenMov = Ellen.transform.position;
+        //Debug.Log(EllenMov);
+    }
+
+    IEnumerator Upload(string jsonData)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("jsonData", jsonData);
+
+
+        UnityWebRequest www = UnityWebRequest.Post(serverUrl, form);
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            //Debug.Log("Form upload complete!");
+            //Debug.Log("Data uploaded successfully!");
+            Debug.Log(www.downloadHandler.text);
+            Debug.Log(jsonData);
+        }
+
+
+    }
 
 }
